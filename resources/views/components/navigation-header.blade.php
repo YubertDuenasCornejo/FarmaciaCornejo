550eb0dfa8234a11fee1d95d666163cbd65485b7
Yubert <yubert.d.c15@gmail.com>
2024-12-25 17:48:49 -0500

    nombre

--- resources/views/components/navigation-header.blade.php
+++ resources/views/components/navigation-header.blade.php
@@ -169,22 +169,20 @@
     <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->name}}</span>
             <img class="img-profile rounded-circle"
-                src="{{ asset('img/undraw_profile.svg') }}">
+                src="{{ asset('img/undraw_profile_1.svg') }}">
         </a>
         <!-- Dropdown - User Information -->
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="userDropdown">
-            <a class="dropdown-item" href="#">
-                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                Perfil
-            </a>
+
             <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="{{route('logout')}}">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                Cerrarrrr sesion
+                Cerrar sesion
             </a>
+            <div class="dropdown-divider"></div>
         </div>
     </li>
 
